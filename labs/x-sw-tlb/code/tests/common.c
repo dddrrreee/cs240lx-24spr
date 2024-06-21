ead6cf95d6deb56fbb92b6bbfdca302aaa20da9d
Ammar Ratnani <ammrat13@gmail.com>
2024-06-21 15:38:45 -0700

    Reduce stack size
    Previously, the program and interrupt stack were both 16MiB large. This is way
    more than we'd need, so I reduced it to 1MiB.

--- labs/x-sw-tlb/code/tests/common.c
+++ labs/x-sw-tlb/code/tests/common.c
@@ -6,19 +6,21 @@
 #include "tests/common.h"
 
 void test_setup(void) {
-    const uint32_t STACK_START = STACK_ADDR - 0x1000000u;
-    const uint32_t INT_STACK_START = INT_STACK_ADDR - 0x1000000u;
+    const uint32_t STACK_START = STACK_ADDR - 0x100000u;
+    const uint32_t INT_STACK_START = INT_STACK_ADDR - 0x100000u;
     unsigned sw_tlb_idx = 0u;
 
     // Everything should be in domain 1
     sw_tlb_mmu_init(0b0100u);
 
-    // Map the code, heap, and stack
+    // Map the code and heap
     pin_t kern_perm =
         pin_16mb(pin_mk_global(dom_kern, perm_rw_priv, MEM_wb_noalloc));
     sw_tlb_set(sw_tlb_idx++, 0x00000000u, 0x00000000u, kern_perm);
+    // Map the stack
+    pin_t stack_perm = pin_mk_global(dom_kern, perm_rw_priv, MEM_wb_noalloc);
     sw_tlb_set(sw_tlb_idx++, STACK_START, STACK_START, kern_perm);
-    sw_tlb_set(sw_tlb_idx++, INT_STACK_START, INT_STACK_START, kern_perm);
+    sw_tlb_set(sw_tlb_idx++, INT_STACK_START, INT_STACK_START, stack_perm);
     // Map devices
     pin_t dev_perm = pin_16mb(pin_mk_device(dom_kern));
     sw_tlb_set(sw_tlb_idx++, 0x20000000u, 0x20000000u, dev_perm);
